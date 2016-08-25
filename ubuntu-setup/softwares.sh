0b6bedc536ef754ed5de4e312051b594dd01e317
Mauricio Minella <mauriciominella@gmail.com>
2016-08-25 20:26:27 -0300

    Atom removed from the setup

--- ubuntu-setup/softwares.sh
+++ ubuntu-setup/softwares.sh
@@ -27,47 +27,8 @@ sudo usermod -aG docker $USER
 
 #docker compose
 sudo touch /usr/local/bin/docker-compose && sudo chown $USER /usr/local/bin/docker-compose
-curl -L https://github.com/docker/compose/releases/download/1.5.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-
-#atom
-sudo add-apt-repository ppa:webupd8team/atom
-sudo apt-get update
-sudo apt-get install atom -y
-
-#atom plugins
-apm install atom-beautify
-apm install atom-typescript
-apm install atom-visual-studio-code-ui
-apm install color-picker
-apm install editorconfig
-apm install file-icons
-apm install git-plus
-apm install highlight-selected
-apm install language-docker
-apm install language-html-swig
-apm install language-nginx
-apm install linter
-apm install linter-eslint
-apm install linter-csslint
-apm install linter-scss-lint
-apm install minimap
-apm install minimap-cursorline
-apm install minimap-find-and-replace
-apm install minimap-git-diff
-apm install minimap-highlight-selected
-apm install minimap-linter
-apm install minimap-selection
-apm install minimap-split-diff
-apm install react
-apm install split-diff
-apm install sync-settings
-apm install tabs-to-spaces
-apm install terminal-panel
-apm install todo-show
-apm install visual-studio-dark-syntax
-apm install webbox-color
-
 
 #terminator
 sudo add-apt-repository ppa:gnome-terminator
